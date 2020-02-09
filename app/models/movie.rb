414da300fb2ece7b734a1f2b80971098edb7517a
ZacheryTrotter <60404480+ZacheryTrotter@users.noreply.github.com>
2020-02-08 22:36:44 -0600

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,5 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-      	result = {}
-      	self.select(:rating).uniq.each do |movie|
-      		result[movie.rating] = 1
-      	end
-      	return result.keys
+      	['G','PG','PG-13','R']
     end
 end
